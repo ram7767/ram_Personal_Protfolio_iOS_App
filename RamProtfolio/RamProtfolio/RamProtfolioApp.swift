b36041e1dbc31c503e1a0ebad6b5340c482d1fa2
ram7767 <rrnram998539@gmail.com>
2023-11-18 11:21:50 +0530

    first commit

---
+++ RamProtfolio/RamProtfolio/RamProtfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamProtfolioApp.swift
+//  RamProtfolio
+//
+//  Created by Softasuave on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RamProtfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SlpashScreenView()
+        }
+    }
+}
---
+++ RamProtfolio/RamProtfolio/View/SlpashScreenView.swift
@@ -0,0 +1,48 @@
+//
+//  SlpashScreenView.swift
+//  RamProtfolio
+//
+//  Created by Softasuave on 17/11/23.
+//
+
+import SwiftUI
+
+struct SlpashScreenView: View {
+    @State var opacity = 0.5
+    @State var isActive = false
+    @State var size = 0.8
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack {
+                    Image(systemName: "person.crop.circle.fill")
+                        .font(.system(size: 80)).padding(.all)
+                    Text("Hi, There...   I'm Ram")
+                        .font(.system(size: 20))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.0)) {
+                        self.size = 0.9
+                    }
+//                    withAnimation(.easeOut(duration: 1.0)) {
+//                        self.size = 0.5
+//                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    isActive = true
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SlpashScreenView()
+}
